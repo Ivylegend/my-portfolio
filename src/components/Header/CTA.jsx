60326754dcd5f5d291ab3f7d8cd78eba68fc3de5
Icha <107883175+Ivylegend@users.noreply.github.com>
2023-10-06 09:19:11 +0100

    Tried adding my Cv for download

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Me from "../../assets/mypics.png";
 // import my cv
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href="" download className="btn">
+      <a href={Me} download className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">Let's Talk</a>
